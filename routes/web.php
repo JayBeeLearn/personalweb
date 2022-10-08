9377acf2f9dfe68241cb19944efe4307cc780b0b
Jahaziel Uko <jaybeelearn@gmail.com>
2022-10-08 16:10:47 +0100

    change home directory

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/jahaziel', [HomeController::class, 'home'])->name('home');
+Route::get('/', [HomeController::class, 'home'])->name('home');
