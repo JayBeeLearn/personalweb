5fa65773cb823604b7cd268fff51218e2e7a8d11
Jahaziel Uko <jaybeelearn@gmail.com>
2023-06-01 00:01:36 +0100

    dist added

--- public/js/script.js
+++ public/js/script.js
@@ -3,7 +3,10 @@ button = document.getElementById('downloadBtn');
 
 function downloadResume() {
     window.print();
-}
+
+    let name = 'dist'
+    console.log(name);
+    }
 
 function nothing() {
     
